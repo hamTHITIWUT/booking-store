48eca48187fe63faed3ef2aa2fd6bb1c27ae9628
thitiwut.kra <thitiwut.kra@turbo.co.th>
2021-08-28 15:13:15 +0700

    init bookstore

---
+++ src/main/java/bookstore/Book.java
@@ -0,0 +1,11 @@
+package bookstore;
+
+public class Book {
+    private int id;
+    private String name;
+    private int price;
+
+    public Book() {
+
+    }
+}
---
+++ src/main/java/bookstore/BookStore.java
@@ -0,0 +1,29 @@
+package bookstore;
+
+import java.util.List;
+
+public class BookStore {
+    private String name;
+
+    public static void main(String[] args) {
+        BookStore bookStore = new BookStore();
+        bookStore.process();
+    }
+
+    private void process() {
+    }
+
+    private void addToBasket() {
+        //TODO
+    }
+
+    private void buy() {
+        //TODO
+    }
+
+    private List<Book> listOfBooks() {
+        //TODO
+        return null;
+    }
+
+}
